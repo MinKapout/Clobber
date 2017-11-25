0dbf6d6267e975ce471db5e6e0a393e08f92a457
MinKapout <arnaud.kevin3@gmail.com>
2017-11-25 20:37:50 +0100

    plateau

--- plateau.py
+++ plateau.py
@@ -3,7 +3,9 @@ from pion import Pion
 class Plateau :
 
     def __init__(self):
-        Plateau = []
-        for PlateauXY in range(3):
-            Plateau.append([0] and [0]*4)
+        self.plateau = []
+        for plateauXY in range(3):
+            self.plateau.append([0] and [0]*4)
 
+    def ajoutPion(self, pion, posX, posY):
+        
